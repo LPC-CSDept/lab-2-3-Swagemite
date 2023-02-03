1d6a2c7db08a78ce21a7f00137037f8d6d1e24c2
Brent Young <bryoung777@icloud.com>
2023-02-02 16:32:48 -0800

    Attempt 1

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
     ##################################################
     # Comlete your code here
+    val1 = int(input("Input your first number: "))
+    val2 = int(input("Input your second number: "))
+    val3 = int(input("Input your third number: "))
+    sum = val1 + val2 + val3
+    avg = sum / 3
+    print("Values: " , val1 , val2 , val3) 
+    print("Totoal: " , sum)
+    print(f'Average \t {avg:.2f}')
+
     ##################################################
 
     pass
